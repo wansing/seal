befde44e1c59f4d5290cf772f9665a3773f2db3b
wansing <55762399+wansing@users.noreply.github.com>
2026-01-26 11:44:17 +0100

    refactor: example main.go

--- cmd/seal/main.go
+++ cmd/seal/main.go
@@ -14,26 +14,24 @@ func main() {
 	listen := "127.0.0.1:8080"
 	reloadSecret := "change-me"
 
-	content := map[string]seal.ContentFunc{
-		".calendar-bs5": content.CalendarBS5{}.Make,
-		".countdown":    content.Countdown,
-		".html":         content.Html,
-		".md":           content.Commonmark,
-	}
-
 	srv := &seal.Server{
-		FS:      os.DirFS("."),
-		Content: content,
+		FS: os.DirFS("."),
+		Content: map[string]seal.ContentFunc{
+			".calendar-bs5": content.CalendarBS5{}.Make,
+			".countdown":    content.Countdown,
+			".html":         content.Html,
+			".md":           content.Commonmark,
+		},
 		Handlers: map[string]seal.HandlerGen{
 			".blog": miniblog.Make,
 		},
 	}
 	srv.Reload()
 
-	log.Printf("listening to %s", listen)
+	http.Handle("/", srv)
 	http.HandleFunc("/errors", srv.ErrorsHandler())
 	http.HandleFunc("/reload", seal.ReloadHandler(reloadSecret, srv.Reload))
 	http.HandleFunc("/git-reload", seal.GitReloadHandler(reloadSecret, ".", srv.Reload))
-	http.Handle("/", srv) // least precendece
+	log.Printf("listening to %s", listen)
 	http.ListenAndServe(listen, nil)
 }
